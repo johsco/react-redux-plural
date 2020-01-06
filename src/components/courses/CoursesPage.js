e9fe1b43f9222b063fb82a7fd3ef5265bfdbbe99
johsco <scott.johnsonbrown@gmail.com>
2020-01-06 11:26:53 -0600

    course 10 complete

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,8 +5,13 @@ import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
+import { Redirect } from "react-router-dom";
 
 class CoursesPage extends React.Component {
+  state = {
+    redirectToAddCoursePage: false
+  };
+
   async componentDidMount() {
     if (this.props.courses.length === 0) {
       try {
@@ -29,6 +34,14 @@ class CoursesPage extends React.Component {
     return (
       <>
         <h2>Courses</h2>
+        {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
+        <button
+          style={{ marginBottom: 20 }}
+          className="btn btn-primary add-course"
+          onClick={() => this.setState({ redirectToAddCoursePage: true })}
+        >
+          Add Course
+        </button>
         <CourseList courses={this.props.courses} />
       </>
     );
